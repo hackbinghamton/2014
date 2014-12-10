95cb3a85cf5e9ad32394d2c8bc015254def1f6ec
chrisbeard <christopher.e.beard@gmail.com>
2014-12-09 20:29:51 -0500

    Update for Spring 2015 hackathon

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function initialize_maps() {
   google.maps.visualRefresh = true;
   map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: new google.maps.LatLng(42.0887882, -75.9719421),
-    zoom: 18,
+    center: new google.maps.LatLng(42.0943754,-75.9585148),
+    zoom: 17,
     mapTypeControl: true,
     scrollwheel:false,
     navigationControl: false,
@@ -16,13 +16,13 @@ function initialize_maps() {
   });
 
   var academicAMarker = new google.maps.Marker({ 
-    position: new google.maps.LatLng(42.088639, -75.972411),
+    position: new google.maps.LatLng(42.0943754,-75.9585148),
     map: map,
     title: "HackBU @ Academic A"
   });
 
   google.maps.event.addListener(academicAMarker, 'click', function() {
-    window.open("https://www.google.com/maps/place/Academic+Bldg+A/@42.0886312,-75.9723588,17z/data=!3m1!4b1!4m2!3m1!1s0x89daeeb84ab6343d:0x7aabe0021d7b218e","_blank");
+    window.open("https://www.google.com/maps/place/Academic+Bldg+A/@42.0943754,-75.9585148,17z/data=!3m1!4b1!4m2!3m1!1s0x89daeeb84ab6343d:0x7aabe0021d7b218e","_blank");
   });
 }
 
